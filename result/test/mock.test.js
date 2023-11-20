e2d5b2ae4db674ce7f4a37bd706efa9ba1af0332
Phillip Eurs <phillip.eurs@panasonic.aero>
2023-11-20 23:24:40 +0000

    adding mock test

--- result/test/mock.test.js
+++ result/test/mock.test.js
@@ -19,11 +19,14 @@ describe('mock test 3', () => {
   });
 });
 
-
 describe('mock test 4', () => {
   it('unit test 4', () => {
     expect(true).to.be.true;
   });
 });
 
-
+describe('mock test 5', () => {
+  it('unit test 5', () => {
+    expect(true).to.be.true;
+  });
+});
